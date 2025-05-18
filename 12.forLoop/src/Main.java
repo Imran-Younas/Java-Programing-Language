e09828a05d7a1c62036f9389e11894c8b1514a3d
Imran <imran.pwr7@gmail.com>
2025-05-18 11:33:31 +0500

    Done

---
+++ 12.forLoop/src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static  void main(String[] args){
+        int num = 5;
+        int tab;
+        for(int i = 1; i<=10; i++){
+            tab = num * i;
+            System.out.println(num + " * " + i + " = " + tab);
+        }
+    }
+}
---
+++ 14.Array/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        int[] marks = {90, 89, 67, 78};
+
+        for (int i=0; i<marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println("\nLength of Array: "+ marks.length);
+        
+    }
+}
